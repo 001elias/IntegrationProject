bc60b9779da2553f7b2bfd28276b0818b00f2d35
eli <ebarrellet@gmail.com>
2024-04-07 22:13:23 -0400

    fixes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-import Navbar from "./components/navbar/Navbar"
+import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
+import Gigs from "./pages/gigs/Gigs";
+import Gig from "./pages/gig/Gig";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Add from "./pages/add/Add";
+import Orders from "./pages/orders/Orders";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import MyGigs from "./pages/myGigs/myGigs";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 function App() {
@@ -25,7 +34,7 @@ function App() {
           path: "/",
           element: <Home />,
         },
-        /* {
+        {
           path: "/gigs",
           element: <Gigs />,
         },
@@ -52,17 +61,17 @@ function App() {
         {
           path: "/gig/:id",
           element: <Gig />,
-        }, */
+        },
       ],
     },
-    /* {
+    {
       path: "/register",
       element: <Register />,
     },
     {
       path: "/login",
       element: <Login />,
-    }, */
+    },
   ]);
 
   return(
