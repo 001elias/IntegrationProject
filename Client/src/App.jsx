58c7c7d26bf8e53249fa08b4b955d75f9b97a3e1
eli <ebarrellet@gmail.com>
2024-04-07 22:03:19 -0400

    Footer

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,76 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 function App() {
 
-  return (
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        /* {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/myGigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        }, */
+      ],
+    },
+    /* {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    }, */
+  ]);
+
+  return(
     <div>
-      <Navbar/>
+    <RouterProvider router={router} />;
     </div>
-  )
+  );
 }
+
 
 export default App
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,8 +5,6 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <Router>
      <App />
-    </Router>
   </React.StrictMode>,
 )
