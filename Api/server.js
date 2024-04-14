274697bfdb68a57607cad8091caa0671ec3193e8
eli <87620595+001elias@users.noreply.github.com>
2024-04-14 17:12:21 -0400

    Requests for img directory

--- Api/server.js
+++ Api/server.js
@@ -56,6 +56,9 @@ app.use((err, req, res, next) => {
 //app.use(express.static(path.join(__dirname, "../Client/dist")));
 app.use(express.static(join(__dirname, "../Client/dist")));
 
+// Serve static files from the src/img directory
+app.use('/src/img', express.static(join(__dirname, "../Client/src/img")));
+
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "../Client/dist", "index.html"));
