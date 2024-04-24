10588a86079c6be989a05d2afe946ab5a12ba241
Dripamine <135563917+Dripamine@users.noreply.github.com>
2024-04-24 19:47:44 -0400

    improve slider readability

--- Client/src/components/catCard/CatCard.jsx
+++ Client/src/components/catCard/CatCard.jsx
@@ -7,8 +7,10 @@ function CatCard({ card, url }) {
     <Link to={url}>
       <div className="catCard">
         <img src={card.img} alt="" />
-        <span className="desc">{card.desc}</span>
-        <span className="title">{card.title}</span>
+        <div className="overlay">
+          <span className="desc">{card.desc}</span>
+          <span className="title">{card.title}</span>
+        </div>
       </div>
     </Link>
   );
